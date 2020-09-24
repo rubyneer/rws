22c17fab7c7510f95e267b890ba41bab7697e97f
Ruby Crutch <rubycrutch@gmail.com>
2020-09-25 00:24:51 +0300

    Initial commit

---
+++ lib/rws.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rws/version'
+
+module RWS
+  class Error < StandardError; end
+end
---
+++ lib/rws/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RWS
+  VERSION = '0.1.0'
+end
